39dd90af39106ad7b89f2eb7251f019e184ed86d
abdirizakmohamedkurewa-web <abdirizakmohamedkurewa@gmail.com>
2025-09-07 09:45:43 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,10 +1,1 @@
-import pkg from 'pg';
-const { Pool } = pkg;
-
-export const pool = new Pool({
-  host: process.env.PGHOST || 'localhost',
-  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
-  database: process.env.PGDATABASE || 'moonlight_db',
-  user: process.env.PGUSER || 'postgres',
-  password: process.env.PGPASSWORD || 'postgres',
-});
+// Database connection file
